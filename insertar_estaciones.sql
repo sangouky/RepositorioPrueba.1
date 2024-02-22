2ef9bdbbd1422ad496ce1fe3cb1625ddf2c341f6
soho <fdavid.bazann@gmail.com>
2024-02-22 09:32:13 -0600

    Agregando por primera vez todo a main

---
+++ insertar_estaciones.sql
@@ -0,0 +1,15 @@
+USE metro;
+
+INSERT INTO `stations` (name) VALUES
+("Lázaro Cárdenas"),
+("Ferrería"),
+("Pantitlán"),
+("Tacuba"),
+("Martín Carrera"),
+("Santa Anita"),
+("Villa de Aragón"),
+("Tezozómoc"),
+("Tecnológico"),
+("Colegio Militar"),
+("Chapultepec"),
+
